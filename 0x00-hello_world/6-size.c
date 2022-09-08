3a07b322557890bf142bed5dc3b0779d01c8ad80
Ahmed Adebayo <adebayoh76@gmail.com>
2022-09-08 08:54:54 -0700

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+    printf("Size of a char: %lu\n", sizeof(char));
+    printf("Size of an int: %lu\n", sizeof(int));
+    printf("Size of an long int: %lu\n", sizeof(long int));
+    printf("Size of an long long int: %lu\n", sizeof(long long int));
+    printf("Size of a float: %lu\n", sizeof(float));
+    return (0);
+}
