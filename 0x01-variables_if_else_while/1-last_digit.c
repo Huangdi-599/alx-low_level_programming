79643ff84ecb642c94b0561342c13190b82bea22
Ahmed Adebayo <adebayoh76@gmail.com>
2022-09-09 12:09:21 -0700

    LOOPS AND DELECRATIONS

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 a = n % 10;
 if (a > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, a);
+printf("Last digit of %d is %d and is greater than 5 ", n, a);
 }
 else if (a == 0)
 {
-printf("Last digit of %d is %d and 0", n, a);
+printf("Last digit of %d is %d and 0 ", n, a);
 }
 if (a < 5)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, a);
+printf("Last digit of %d is %d and is less than 6 and not 0 ", n, a);
 }
 return (0);
 }
