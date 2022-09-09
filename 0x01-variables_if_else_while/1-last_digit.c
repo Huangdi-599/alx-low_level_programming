4d2fbb9171c18bf66ae3c8e97fe1a0ef390b2762
Ahmed Adebayo <adebayoh76@gmail.com>
2022-09-09 12:27:26 -0700

    LOOPS AND DELECRATIONS

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and 0\n", n, a);
 }
 if (a < 6 && a != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+printf("Last digit of %d is %d and is less than 6 and not is 0\n", n, a);
 }
 return (0);
 }
