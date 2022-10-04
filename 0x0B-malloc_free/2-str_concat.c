03b24cbb04c6d288409357d15c865a7f1e5cf9ef
Ahmed Adebayo <adebayoh76@gmail.com>
2022-10-04 12:55:20 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,27 +9,22 @@
 *Return: NULL in case of an empty string , but pointer to new string in
 *case of success
 */
-
 char *str_concat(char *s1, char *s2)
 {
-char *concat;
-int j, l1, l2, k, index = 0;
+char *concat_str;
+int index, concat_index = 0,  len = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (l1 = 0; l1 <= *s1; l1++)
-{
-}
-for (l2 = 0; l2 <= *s2; l2++)
-{
-}
-concat = malloc(sizeof(char) * (l1 + l2 + 1));
-if (concat == NULL)
+for (index = 0; s1[index] || s2[index]; index++)
+len++;
+concat_str = malloc(sizeof(char) * len);
+if (concat_str == NULL)
 return (NULL);
-for (j = 0; s1[j]; j++)
-concat[index++] = s1[j];
-for (k = 0; s2[k]; k++)
-concat[index++] = s2[k];
-return (concat);
+for (index = 0; s1[index]; index++)
+concat_str[concat_index++] = s1[index];
+for (index = 0; s2[index]; index++)
+concat_str[concat_index++] = s2[index];
+return (concat_str);
 }
