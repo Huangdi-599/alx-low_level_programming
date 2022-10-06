e6ac8422a5543cce4a68d8cd5ca4e99d49f41d8a
Ahmed Adebayo <adebayoh76@gmail.com>
2022-10-06 12:12:40 -0700

    MORE ON MALLOC, CALLOC AND REALLOC

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,29 +15,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *concat;
-unsigned int i, j, loop1, loop2;
+unsigned int len = n, index;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (i = 0; s1[i] != '\0'; i++)
-{
-}
-for (j = 0; s2[j] != '\0'; j++)
-{
-}
-j += i;
-concat = malloc(sizeof(char *) * (j + 1));
+for (index = 0; s1[index]; index++)
+len++;
+concat = malloc(sizeof(char) * (len + 1));
 if (concat == NULL)
 return (NULL);
-for (loop1 = 0; loop1 < i; loop1++)
-	concat[loop1] = s1[loop1];
-for (loop2 = 0; loop2 < j && loop2 < n; loop2++)
-{
-concat[loop1] = s2[loop2];
-loop1++;
-}
-loop1++;
-concat[loop1] = '\0';
+len = 0;
+for (index = 0; s1[index]; index++)
+concat[len++] = s1[index];
+for (index = 0; s2[index] && index < n; index++)
+concat[len++] = s2[index];
+concat[len] = '\0';
 return (concat);
 }
