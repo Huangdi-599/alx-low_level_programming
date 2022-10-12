706b2dd34a617174b892d4ada9f934fcbce628b7
Ahmed Adebayo <adebayoh76@gmail.com>
2022-10-12 10:36:01 -0700

    FUNCTION POINTER

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,7 +17,7 @@ op_t op_s[] = {
 {NULL, NULL}
 };
 int i = 0;
-while (ops[i].op != NULL && *(ops[i].op) != *s)
+while (op_s[i].op != NULL && *(op_s[i].op) != *s)
 i++;
-return (ops[i].f);
+return (op_s[i].f);
 }
