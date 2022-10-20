10bdd78ca31894c516bb4f8f8f8160f878070c5d
Ahmed Adebayo <adebayoh76@gmail.com>
2022-10-20 12:32:22 -0700

    LINKED LISTS

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
-#include "lists.h"
 #include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+
 /**
- * constructorRun - runs
- *  before main function is executed.
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void constructorRun(void)
+void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+	       "I bore my house upon my back!\n");
 }
