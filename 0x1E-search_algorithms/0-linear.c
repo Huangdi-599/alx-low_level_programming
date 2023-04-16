8b62f6815f09f9b618f0c85328638973ece24d1d
Huangdi-599 <adebayoh76@gmail.com>
2023-04-16 16:52:13 -0700

    Donee

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,6 +7,8 @@
  * @array: input array
  * @size: size of the array
  * @value: value to search in
+ * Description: Prints a value every time it is compared in the array.
+ * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
 {
